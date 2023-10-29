cac4aa1c666a86e09613a38a4eac5c9d120e1bdd
unfunnydude <eroollsaqipii@gmail.com>
2023-10-29 18:32:29 +0100

    done

---
+++ LAB1/lab/src/main/java/mk/finki/ukim/mk/lab/service/impl/MoveServiceImpl.java
@@ -0,0 +1,30 @@
+package mk.finki.ukim.mk.lab.service.impl;
+
+import mk.finki.ukim.mk.lab.model.*;
+import mk.finki.ukim.mk.lab.repository.*;
+import mk.finki.ukim.mk.lab.service.*;
+import org.springframework.stereotype.*;
+
+import java.util.*;
+
+
+@Service
+public class MoveServiceImpl  implements MovieService {
+
+    private final InMemoryMovieRepository inMemoryMovieRepository;
+
+    public MoveServiceImpl(InMemoryMovieRepository inMemoryMovieRepository) {
+        this.inMemoryMovieRepository = inMemoryMovieRepository;
+    }
+
+    @Override
+    public List<Movie> listAll() {
+        return inMemoryMovieRepository.findAll();
+    }
+
+    @Override
+    public List<Movie> searchMovies(String text, String rating) {
+        return inMemoryMovieRepository.searchMovies(text, rating);
+    }
+
+}
